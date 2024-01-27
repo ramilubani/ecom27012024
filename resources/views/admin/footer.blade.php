869054cb2e3e85edf831d439d3ed8cf631065889
ramilubani <ramilubani@.com>
2024-01-27 17:17:54 +0100

    added app

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,6 @@
+ <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © Web Tech Knowledge 2022</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">  <a href="https://www.youtube.com/c/WebTechKnowledge" target="_blank">Web Tech Knowledge</a>  </span>
+            </div>
+          </footer>
